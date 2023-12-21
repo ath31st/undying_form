86ee374e6d4f08a4198ab3b4f22cfb4c975abf02
Vladimir Olennikov <ath31st@yandex.ru>
2023-12-22 00:01:30 +0300

    migrate from sqlite to postgres v2, update application.yaml and sql script

--- src/main/resources/db/migration/V001__create_table_users.sql
+++ src/main/resources/db/migration/V001__create_table_users.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users
 (
-    id             INTEGER PRIMARY KEY,
+    id             BIGSERIAL PRIMARY KEY,
     username       VARCHAR(300) UNIQUE NOT NULL,
     name           VARCHAR(300) NOT NULL,
     register_date  VARCHAR(100) NOT NULL,
