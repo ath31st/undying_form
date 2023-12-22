545d09ecd11e17b22c1401ee6156a5a36368d946
Vladimir Olennikov <ath31st@yandex.ru>
2023-12-22 17:14:20 +0300

    add caretakers table and inserts

---
+++ src/main/resources/db/migration/V008__create_table_caretakers.sql
@@ -0,0 +1,11 @@
+CREATE TABLE caretakers
+(
+    caretaker_id          SERIAL PRIMARY KEY,
+    name                  VARCHAR(255) NOT NULL,
+    age                   INT,
+    weapon_type           VARCHAR(50),
+    is_alive              BOOLEAN,
+    successful_defenses   INT,
+    unsuccessful_defenses INT,
+    graveyard_id          INT REFERENCES graveyards (graveyard_id)
+);
