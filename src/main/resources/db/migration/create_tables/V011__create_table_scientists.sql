5ea31967239c27d3ad4f7c38f683c6155f6abb99
Vladimir Olennikov <ath31st@yandex.ru>
2023-12-26 18:17:58 +0300

    update scientists table

--- src/main/resources/db/migration/create_tables/V011__create_table_scientists.sql
+++ src/main/resources/db/migration/create_tables/V011__create_table_scientists.sql
@@ -3,8 +3,12 @@ CREATE TABLE scientists
     scientist_id           BIGSERIAL PRIMARY KEY,
     name                   VARCHAR(255) NOT NULL,
     age                    INT,
-    health                 INT,
+    physical_health        INT,
+    mental_health          INT,
     experience             INT,
+    alchemy                INT,
+    biology                INT,
+    engineering            INT,
     successful_experiments INT
 --     education_id           INT REFERENCES education (education_id),
 --     specialization_id      INT REFERENCES specializations (specialization_id),
