4aeb0437aeb5eba2d16a21ae1eb0522ebb23d2af
Vladimir Olennikov <ath31st@yandex.ru>
2023-12-27 15:36:22 +0300

    add hideouts table

---
+++ src/main/resources/db/migration/create_tables/V026__create_table_hideouts.sql
@@ -0,0 +1,11 @@
+CREATE TABLE hideouts
+(
+    hideout_id            BIGSERIAL PRIMARY KEY,
+    scientist_id          BIGSERIAL REFERENCES scientists (scientist_id),
+    district_id           SERIAL REFERENCES districts (district_id),
+    name                  VARCHAR(255) NOT NULL,
+    description           TEXT         NOT NULL,
+    alchemy_equipment     INT,
+    biology_equipment     INT,
+    engineering_equipment INT
+);
