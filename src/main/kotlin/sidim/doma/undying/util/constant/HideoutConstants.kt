8df54deb47e9bf5f78c810a08d1426bc397c3ee6
Vladimir Olennikov <ath31st@yandex.ru>
2023-12-29 11:15:34 +0300

    add constant package, add constants for HideoutService and ScientistService

---
+++ src/main/kotlin/sidim/doma/undying/util/constant/HideoutConstants.kt
@@ -0,0 +1,6 @@
+package sidim.doma.undying.util.constant
+
+object HideoutConstants {
+    const val COUNT_EQUIPMENT = 3
+    const val BONUSES_SUM = 12
+}
---
+++ src/main/kotlin/sidim/doma/undying/util/constant/ScientistConstants.kt
@@ -0,0 +1,11 @@
+package sidim.doma.undying.util.constant
+
+object ScientistConstants {
+    const val DEFAULT_PHYSICAL_HEALTH = 100
+    const val DEFAULT_MENTAL_HEALTH = 100
+    const val DEFAULT_EXPERIENCE = 0
+    const val SKILLS_COUNT = 3
+    const val BONUSES_SUM = 12
+    const val MIN_AGE = 18
+    const val MAX_AGE = 60
+}
