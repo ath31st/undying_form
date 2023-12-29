208b4afe3add8b1502b95186cc8e040e64ce8b84
Vladimir Olennikov <ath31st@yandex.ru>
2023-12-29 11:16:06 +0300

    replace magic numbers from HideoutService and ScientistService

--- src/main/kotlin/sidim/doma/undying/service/HideoutService.kt
+++ src/main/kotlin/sidim/doma/undying/service/HideoutService.kt
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Service
 import sidim.doma.undying.generated.tables.pojos.Hideouts
 import sidim.doma.undying.repository.HideoutRepository
 import sidim.doma.undying.util.GeneratorRandomValuesUtil
+import sidim.doma.undying.util.constant.HideoutConstants.BONUSES_SUM
+import sidim.doma.undying.util.constant.HideoutConstants.COUNT_EQUIPMENT
 
 @Service
 class HideoutService(
@@ -14,8 +16,8 @@ class HideoutService(
 ) {
     fun createHideout(): Hideouts {
         val name = namingService.generateHideoutName()
-        val countEquipment = 3
-        val sumBonuses = 8
+        val countEquipment = COUNT_EQUIPMENT
+        val sumBonuses = BONUSES_SUM
         val randomValues =
             generatorRandomValuesUtil.generateRandomValues(countEquipment, sumBonuses)
         val districtId = districtService.getRandomDistrictId()
--- src/main/kotlin/sidim/doma/undying/service/ScientistService.kt
+++ src/main/kotlin/sidim/doma/undying/service/ScientistService.kt
@@ -5,32 +5,40 @@ import sidim.doma.undying.dto.scientist.ScientistDto
 import sidim.doma.undying.generated.tables.pojos.Scientists
 import sidim.doma.undying.repository.ScientistRepository
 import sidim.doma.undying.util.GeneratorRandomValuesUtil
+import sidim.doma.undying.util.constant.ScientistConstants.BONUSES_SUM
+import sidim.doma.undying.util.constant.ScientistConstants.DEFAULT_EXPERIENCE
+import sidim.doma.undying.util.constant.ScientistConstants.DEFAULT_MENTAL_HEALTH
+import sidim.doma.undying.util.constant.ScientistConstants.DEFAULT_PHYSICAL_HEALTH
+import sidim.doma.undying.util.constant.ScientistConstants.MAX_AGE
+import sidim.doma.undying.util.constant.ScientistConstants.MIN_AGE
+import sidim.doma.undying.util.constant.ScientistConstants.SKILLS_COUNT
 
 @Service
 class ScientistService(
     private val scientistRepository: ScientistRepository,
     private val hideoutService: HideoutService,
     private val namingService: NamingService,
+    private val educationService: EducationService,
     private val generatorRandomValuesUtil: GeneratorRandomValuesUtil
 ) {
     fun createScientist(): Scientists {
-        val countSkills = 3
-        val sumBonuses = 12
+        val countSkills = SKILLS_COUNT
+        val sumBonuses = BONUSES_SUM
         val randomValues =
             generatorRandomValuesUtil.generateRandomValues(countSkills, sumBonuses)
         val dto = ScientistDto(
             name = namingService.generateCaretakerFirstLastName(),
-            age = generatorRandomValuesUtil.generateRandomInteger(18, 60),
-            physicalHealth = 100,
-            mentalHealth = 100,
-            experience = 0,
+            age = generatorRandomValuesUtil.generateRandomInteger(MIN_AGE, MAX_AGE),
+            physicalHealth = DEFAULT_PHYSICAL_HEALTH,
+            mentalHealth = DEFAULT_MENTAL_HEALTH,
+            experience = DEFAULT_EXPERIENCE,
             biology = randomValues[0],
             alchemy = randomValues[1],
             engineering = randomValues[2],
             successfulExperiments = 0,
-            educationId = 1, // todo add this random education
+            educationId = educationService.getRandomEducationId(),
             specializationId = null,
-            hideoutId = hideoutService.createHideout().hideoutId!!
+            hideoutId = hideoutService.createHideout().hideoutId.let { it ?: 0 }
         )
         return scientistRepository.createScientist(dto)
     }
