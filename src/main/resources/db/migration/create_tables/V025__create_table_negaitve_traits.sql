0323d293cb2a5464cf580cec71ca3c7cd726e43f
Vladimir Olennikov <ath31st@yandex.ru>
2024-01-01 23:31:45 +0300

    shift scripts by two

---
+++ src/main/resources/db/migration/create_tables/V025__create_table_negaitve_traits.sql
@@ -0,0 +1,13 @@
+CREATE TABLE negative_traits
+(
+    negative_trait_id       SERIAL PRIMARY KEY,
+    name                    VARCHAR(255)                                     NOT NULL,
+    description             TEXT                                             NOT NULL,
+    alchemy_penalty         INT,
+    biology_penalty         INT,
+    engineering_penalty     INT,
+    physical_health_penalty INT,
+    mental_health_penalty   INT,
+    weight                  INT,
+    trait_group_id          INTEGER REFERENCES trait_groups (trait_group_id) NOT NULL
+);
