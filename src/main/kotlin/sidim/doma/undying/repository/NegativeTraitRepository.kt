94765048e0138ccf898a8136d24e714920581f6a
Vladimir Olennikov <ath31st@yandex.ru>
2024-01-02 22:24:34 +0300

    add save methods in traits repositories

--- src/main/kotlin/sidim/doma/undying/repository/NegativeTraitRepository.kt
+++ src/main/kotlin/sidim/doma/undying/repository/NegativeTraitRepository.kt
@@ -3,12 +3,26 @@ package sidim.doma.undying.repository
 import org.jooq.DSLContext
 import org.springframework.stereotype.Repository
 import sidim.doma.undying.generated.tables.pojos.NegativeTraits
+import sidim.doma.undying.generated.tables.records.ScientistNegativeTraitsRecord
 import sidim.doma.undying.generated.tables.references.NEGATIVE_TRAITS
+import sidim.doma.undying.generated.tables.references.SCIENTIST_NEGATIVE_TRAITS
 
 @Repository
 class NegativeTraitRepository(private val dslContext: DSLContext) {
     fun getAllTraits(): List<NegativeTraits> {
         return dslContext.selectFrom(NEGATIVE_TRAITS)
             .fetchInto(NegativeTraits::class.java)
+    }
+
+    fun saveScientistNegativeTraits(scientistId: Long, traitIds: List<Int>) {
+        val records = mutableListOf<ScientistNegativeTraitsRecord>()
+        traitIds.forEach { id ->
+            val r = dslContext.newRecord(SCIENTIST_NEGATIVE_TRAITS)
+            r.scientistId = scientistId
+            r.negativeTraitId = id
+            records.add(r)
+        }
+
+        dslContext.batchInsert(records).execute()
     }
 }
--- src/main/kotlin/sidim/doma/undying/repository/PositiveTraitRepository.kt
+++ src/main/kotlin/sidim/doma/undying/repository/PositiveTraitRepository.kt
@@ -3,12 +3,26 @@ package sidim.doma.undying.repository
 import org.jooq.DSLContext
 import org.springframework.stereotype.Repository
 import sidim.doma.undying.generated.tables.pojos.PositiveTraits
+import sidim.doma.undying.generated.tables.records.ScientistPositiveTraitsRecord
 import sidim.doma.undying.generated.tables.references.POSITIVE_TRAITS
+import sidim.doma.undying.generated.tables.references.SCIENTIST_POSITIVE_TRAITS
 
 @Repository
 class PositiveTraitRepository(private val dslContext: DSLContext) {
     fun getAllTraits(): List<PositiveTraits> {
         return dslContext.selectFrom(POSITIVE_TRAITS)
             .fetchInto(PositiveTraits::class.java)
+    }
+
+    fun saveScientistPositiveTraits(scientistId: Long, traitIds: List<Int>) {
+        val records = mutableListOf<ScientistPositiveTraitsRecord>()
+        traitIds.forEach { id ->
+            val r = dslContext.newRecord(SCIENTIST_POSITIVE_TRAITS)
+            r.scientistId = scientistId
+            r.positiveTraitId = id
+            records.add(r)
+        }
+
+        dslContext.batchInsert(records).execute()
     }
 }
