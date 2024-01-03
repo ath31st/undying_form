2181bb34141c34c08c1568689ae4042e49a64eb3
Vladimir Olennikov <ath31st@yandex.ru>
2024-01-03 10:26:59 +0300

    add traits controller

---
+++ src/main/kotlin/sidim/doma/undying/controller/TraitController.kt
@@ -0,0 +1,36 @@
+package sidim.doma.undying.controller
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import sidim.doma.undying.service.TraitService
+
+@RestController
+@Validated
+@RequestMapping("/api/v1/traits")
+class TraitController(
+    private val traitService: TraitService
+) {
+    @PostMapping("/new_pos_trait")
+    fun createPositiveTrait(@RequestBody dto: NewTraitDto): ResponseEntity<HttpStatus> {
+        traitService.createPositiveTrait(dto)
+        return ResponseEntity(HttpStatus.CREATED)
+    }
+
+    @PostMapping("/new_neg_trait")
+    fun createNegativeTrait(@RequestBody dto: NewTraitDto): ResponseEntity<HttpStatus> {
+        traitService.createNegativeTrait(dto)
+        return ResponseEntity(HttpStatus.CREATED)
+    }
+
+    @GetMapping("/scientist/{id}")
+    fun scientistTraits(@PathVariable id: Long): ResponseEntity<ScientistTraitsDto> {
+        return ResponseEntity.ok(traitService.getTraitsByScientistId(id))
+    }
+}
