02e509aac181c723f79bd40f1e5315b1311485ff
Vladimir Olennikov <ath31st@yandex.ru>
2024-01-05 15:17:33 +0300

    fix some warnings and naming

--- src/main/kotlin/sidim/doma/undying/exceptionhandler/exception/EducationException.kt
+++ src/main/kotlin/sidim/doma/undying/exceptionhandler/exception/EducationException.kt
@@ -2,5 +2,9 @@ package sidim.doma.undying.exceptionhandler.exception
 
 import org.springframework.http.HttpStatus
 
-class EducationException(message: String? = null, val status: HttpStatus, cause: Throwable? = null) :
+class EducationException(
+    message: String? = null,
+    val status: HttpStatus,
+    cause: Throwable? = null
+) :
     RuntimeException(message, cause)
