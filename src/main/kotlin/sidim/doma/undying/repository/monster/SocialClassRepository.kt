94d57aac4d40e98aefb4be2074c4c4b24cea2252
Vladimir Olennikov <ath31st@yandex.ru>
2024-01-07 20:07:47 +0300

    realize methods for creating and saving new social class

--- src/main/kotlin/sidim/doma/undying/repository/monster/SocialClassRepository.kt
+++ src/main/kotlin/sidim/doma/undying/repository/monster/SocialClassRepository.kt
@@ -3,12 +3,23 @@ package sidim.doma.undying.repository.monster
 import org.jooq.DSLContext
 import org.jooq.impl.DSL
 import org.springframework.stereotype.Repository
+import sidim.doma.undying.dto.NewSocialClassDto
+import sidim.doma.undying.generated.tables.pojos.SocialClasses
 import sidim.doma.undying.generated.tables.references.SOCIAL_CLASSES
 
 @Repository
 class SocialClassRepository(private val dslContext: DSLContext) {
     private val sc = SOCIAL_CLASSES
-    
+
+    fun createSocialClass(dto: NewSocialClassDto): SocialClasses {
+        val r = dslContext.newRecord(sc)
+        r.name = dto.name
+        r.description = dto.description
+
+        r.store()
+        return r.into(SocialClasses::class.java)
+    }
+
     fun getRandomSocialClassId(): Int? {
         return dslContext.select(sc.SOCIAL_CLASS_ID)
             .from(sc)
--- src/main/kotlin/sidim/doma/undying/service/monster/SocialClassService.kt
+++ src/main/kotlin/sidim/doma/undying/service/monster/SocialClassService.kt
@@ -1,8 +1,13 @@
 package sidim.doma.undying.service.monster
 
 import org.springframework.stereotype.Service
+import sidim.doma.undying.dto.NewSocialClassDto
+import sidim.doma.undying.generated.tables.pojos.SocialClasses
 import sidim.doma.undying.repository.monster.SocialClassRepository
 
 @Service
 class SocialClassService(private val socialClassRepository: SocialClassRepository) {
+    fun createSocialClass(dto: NewSocialClassDto): SocialClasses {
+        return socialClassRepository.createSocialClass(dto)
+    }
 }
