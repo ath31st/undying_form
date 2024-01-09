068f736ec65e1aeb94814f0cec5d024b02decd27
Vladimir Olennikov <ath31st@yandex.ru>
2024-01-09 19:01:51 +0300

    fix a type for ids in sql script

--- src/main/resources/db/migration/create_tables/V037__create_table_m_to_m_graveyards_social_classes.sql
+++ src/main/resources/db/migration/create_tables/V037__create_table_m_to_m_graveyards_social_classes.sql
@@ -1,6 +1,6 @@
 CREATE TABLE graveyard_social_classes
 (
-    graveyard_id    INT REFERENCES graveyards (graveyard_id),
-    social_class_id INT REFERENCES social_classes (social_class_id),
+    graveyard_id    SERIAL REFERENCES graveyards (graveyard_id),
+    social_class_id SERIAL REFERENCES social_classes (social_class_id),
     PRIMARY KEY (graveyard_id, social_class_id)
 );
