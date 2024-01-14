4b59220cb1c6fa7c756e7c01f4b61f791d1524dd
ath31st <ath31st@yandex.ru>
2024-01-14 10:23:54 +0300

    add package bodyparts and first dto - NewHandDto

---
+++ src/main/kotlin/sidim/doma/undying/dto/bodyparts/NewHandDto.kt
@@ -0,0 +1,9 @@
+package sidim.doma.undying.dto.bodyparts
+
+class NewHandDto(
+    val quality: Int,
+    val integrity: Int,
+    val side: String,
+    val handTemplateId: Int,
+    val storageId: Long,
+)
