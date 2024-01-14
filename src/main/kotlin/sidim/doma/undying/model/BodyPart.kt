cb097c5665a68f97feb3b787bd51dde7c5b92840
ath31st <ath31st@yandex.ru>
2024-01-14 13:13:22 +0300

    update body part models

--- src/main/kotlin/sidim/doma/undying/model/BodyPart.kt
+++ src/main/kotlin/sidim/doma/undying/model/BodyPart.kt
@@ -4,16 +4,18 @@ interface BodyPart {
     val id: Long
     val quality: Int
     val integrity: Int
-    val storageId: Int?
-    val setBodyPartsId: Int?
+    val storageId: Long?
+    val setBodyPartsId: Long?
+    val bodyPartTemplateId: Int
 }
 
 data class Hand(
     override val id: Long,
     override val quality: Int,
     override val integrity: Int,
-    override val storageId: Int?,
-    override val setBodyPartsId: Int?,
+    override val storageId: Long?,
+    override val setBodyPartsId: Long?,
+    override val bodyPartTemplateId: Int,
     val side: String,
 ) : BodyPart
 
@@ -21,8 +23,9 @@ data class Leg(
     override val id: Long,
     override val quality: Int,
     override val integrity: Int,
-    override val storageId: Int?,
-    override val setBodyPartsId: Int?,
+    override val storageId: Long?,
+    override val setBodyPartsId: Long?,
+    override val bodyPartTemplateId: Int,
     val side: String,
 ) : BodyPart
 
@@ -30,14 +33,16 @@ data class UpperBody(
     override val id: Long,
     override val quality: Int,
     override val integrity: Int,
-    override val storageId: Int?,
-    override val setBodyPartsId: Int?,
+    override val storageId: Long?,
+    override val setBodyPartsId: Long?,
+    override val bodyPartTemplateId: Int,
 ) : BodyPart
 
 data class Head(
     override val id: Long,
     override val quality: Int,
     override val integrity: Int,
-    override val storageId: Int?,
-    override val setBodyPartsId: Int?,
+    override val storageId: Long?,
+    override val setBodyPartsId: Long?,
+    override val bodyPartTemplateId: Int,
 ) : BodyPart
