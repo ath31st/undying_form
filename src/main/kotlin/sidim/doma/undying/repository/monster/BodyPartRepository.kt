ce5699acdb9116b22ec94d8c11c08b42da91c42a
ath31st <ath31st@yandex.ru>
2024-01-14 10:25:40 +0300

    add methods for saving and generating hands

--- src/main/kotlin/sidim/doma/undying/repository/monster/BodyPartRepository.kt
+++ src/main/kotlin/sidim/doma/undying/repository/monster/BodyPartRepository.kt
@@ -2,6 +2,7 @@ package sidim.doma.undying.repository.monster
 
 import org.jooq.DSLContext
 import org.springframework.stereotype.Repository
+import sidim.doma.undying.dto.bodyparts.NewHandDto
 import sidim.doma.undying.generated.tables.references.HANDS
 import sidim.doma.undying.generated.tables.references.HEADS
 import sidim.doma.undying.generated.tables.references.LEGS
@@ -16,6 +17,18 @@ class BodyPartRepository(private val dslContext: DSLContext) {
     private val ub = UPPER_BODIES
     private val he = HEADS
     private val s = STORAGES
+
+    fun saveGeneratedHandInStorage(dto: NewHandDto): Hand {
+        val r = dslContext.newRecord(ha)
+        r.quality = dto.quality
+        r.integrity = dto.integrity
+        r.side = dto.side
+        r.handTemplateId = dto.handTemplateId
+        r.storageId = dto.storageId
+
+        r.store()
+        return r.into(Hand::class.java)
+    }
 
     fun findHandsByStorageId(storageId: Long): List<Hand> {
         return dslContext.select(ha)
--- src/main/kotlin/sidim/doma/undying/service/monster/BodyPartService.kt
+++ src/main/kotlin/sidim/doma/undying/service/monster/BodyPartService.kt
@@ -1,18 +1,34 @@
 package sidim.doma.undying.service.monster
 
+import kotlin.reflect.KFunction2
 import org.springframework.stereotype.Service
+import sidim.doma.undying.dto.bodyparts.NewHandDto
 import sidim.doma.undying.model.BodyPart
 import sidim.doma.undying.model.Hand
 import sidim.doma.undying.repository.monster.BodyPartRepository
+import sidim.doma.undying.util.GeneratorRandomValuesUtil
 
 @Service
 class BodyPartService(
     private val bodyPartRepository: BodyPartRepository,
     private val bodyPartsTemplateService: BodyPartsTemplateService,
+    private val generator: GeneratorRandomValuesUtil,
 ) {
-    fun generateRandomBodyPartsByGraveyardId(graveyardId: Int) {
-        val check = bodyPartsTemplateService.getRandomHandTemplateIdByGraveyardId(graveyardId)
-        println(check)
+    fun generateRandomHandByGraveyardId(graveyardId: Int, storageId: Long): Hand {
+        val handDto = NewHandDto(
+            quality = generator.generateRandomInteger(1, 3), // TODO rework this generation
+            integrity = generator.generateRandomInteger(85, 100), // TODO rework this generation
+            side = listOf("left", "right").random(),
+            handTemplateId = bodyPartsTemplateService.getRandomHandTemplateIdByGraveyardId(graveyardId),
+            storageId = storageId
+        )
+
+        return bodyPartRepository.saveGeneratedHandInStorage(handDto)
+    }
+
+    fun generateRandomBodyPartsByGraveyardIdAndSaveInStorage(graveyardId: Int, storageId: Long, countBodyParts: Int) {
+        val generateBodyParts: List<KFunction2<Int, Long, Hand>> =
+            listOf(::generateRandomHandByGraveyardId)
     }
 
     fun findBodyPartsByStorageId(storageId: Long): List<BodyPart> {
