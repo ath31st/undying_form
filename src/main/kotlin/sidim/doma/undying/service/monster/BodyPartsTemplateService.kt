69e60e3828f08f2968fd48372b809596df5da47c
ath31st <ath31st@yandex.ru>
2024-01-15 09:54:57 +0300

    refactoring body parts template service methods

--- src/main/kotlin/sidim/doma/undying/service/monster/BodyPartsTemplateService.kt
+++ src/main/kotlin/sidim/doma/undying/service/monster/BodyPartsTemplateService.kt
@@ -8,34 +8,35 @@ import sidim.doma.undying.repository.monster.BodyPartsTemplateRepository
 @Service
 class BodyPartsTemplateService(private val bodyPartsTemplateRepository: BodyPartsTemplateRepository) {
     fun getRandomHandTemplateIdByGraveyardId(graveyardId: Int): Int {
-        return bodyPartsTemplateRepository.getRandomHandTemplateIdByGraveyardId(graveyardId)
-            ?: throw BodyPartsTemplateException(
-                "Graveyard with id: $graveyardId does not have compatible body parts templates",
-                HttpStatus.NOT_FOUND
-            )
+        val handTemplateId = bodyPartsTemplateRepository.getRandomHandTemplateIdByGraveyardId(graveyardId)
+        checkExistsBodyPartsTemplate(handTemplateId, graveyardId)
+        return handTemplateId!!
     }
 
     fun getRandomLegTemplateIdByGraveyardId(graveyardId: Int): Int {
-        return bodyPartsTemplateRepository.getRandomLegTemplateIdByGraveyardId(graveyardId)
-            ?: throw BodyPartsTemplateException(
-                "Graveyard with id: $graveyardId does not have compatible body parts templates",
-                HttpStatus.NOT_FOUND
-            )
+        val legTemplateId = bodyPartsTemplateRepository.getRandomLegTemplateIdByGraveyardId(graveyardId)
+        checkExistsBodyPartsTemplate(legTemplateId, graveyardId)
+        return legTemplateId!!
     }
 
     fun getRandomUpperBodyTemplateIdByGraveyardId(graveyardId: Int): Int {
-        return bodyPartsTemplateRepository.getRandomUpperBodyTemplateIdByGraveyardId(graveyardId)
-            ?: throw BodyPartsTemplateException(
-                "Graveyard with id: $graveyardId does not have compatible body parts templates",
-                HttpStatus.NOT_FOUND
-            )
+        val upperBodyTemplateId = bodyPartsTemplateRepository.getRandomUpperBodyTemplateIdByGraveyardId(graveyardId)
+        checkExistsBodyPartsTemplate(upperBodyTemplateId, graveyardId)
+        return upperBodyTemplateId!!
     }
 
     fun getRandomHeadTemplateIdByGraveyardId(graveyardId: Int): Int {
-        return bodyPartsTemplateRepository.getRandomHeadTemplateIdByGraveyardId(graveyardId)
-            ?: throw BodyPartsTemplateException(
+        val headTemplateId = bodyPartsTemplateRepository.getRandomHeadTemplateIdByGraveyardId(graveyardId)
+        checkExistsBodyPartsTemplate(headTemplateId, graveyardId)
+        return headTemplateId!!
+    }
+
+    private fun checkExistsBodyPartsTemplate(templateId: Int?, graveyardId: Int) {
+        if (templateId == null) {
+            throw BodyPartsTemplateException(
                 "Graveyard with id: $graveyardId does not have compatible body parts templates",
                 HttpStatus.NOT_FOUND
             )
+        }
     }
 }
