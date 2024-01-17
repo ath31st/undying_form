f8855e96741c6ee30d04e464213563d78fcda205
ath31st <ath31st@yandex.ru>
2024-01-17 15:56:47 +0300

    change tables for simplify body parts schema v3 fix absent field

--- src/main/resources/db/migration/create_tables/V033__create_table_body_parts.sql
+++ src/main/resources/db/migration/create_tables/V033__create_table_body_parts.sql
@@ -3,5 +3,6 @@ CREATE TABLE body_parts
     body_part_id          BIGSERIAL PRIMARY KEY,
     quality               INTEGER NOT NULL CHECK (quality >= 0),
     integrity             INTEGER NOT NULL CHECK (integrity >= 0),
+    side                  VARCHAR(10) CHECK (side IN ('left', 'right')),
     body_part_template_id INTEGER REFERENCES body_part_templates (body_part_template_id)
 );
