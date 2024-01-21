9f034550fbce306a54473abaa9f0c01b0eb00f82
ath31st <ath31st@yandex.ru>
2024-01-21 16:02:57 +0300

    add support functions for BodyPartRepository

--- src/main/kotlin/sidim/doma/undying/repository/monster/BodyPartRepository.kt
+++ src/main/kotlin/sidim/doma/undying/repository/monster/BodyPartRepository.kt
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository
 import sidim.doma.undying.dto.bodyparts.NewBodyPartDto
 import sidim.doma.undying.generated.tables.references.BODY_PARTS
 import sidim.doma.undying.generated.tables.references.BODY_PART_TEMPLATES
+import sidim.doma.undying.generated.tables.references.SCHOLARS
 import sidim.doma.undying.generated.tables.references.SOCIAL_CLASSES
 import sidim.doma.undying.generated.tables.references.STORAGES
 import sidim.doma.undying.mapper.BodyPartMapper
@@ -14,8 +15,9 @@ import sidim.doma.undying.model.BodyPart
 class BodyPartRepository(private val dslContext: DSLContext, private val bodyPartMapper: BodyPartMapper) {
     private val bp = BODY_PARTS
     private val bpt = BODY_PART_TEMPLATES
-    private val s = STORAGES
+    private val st = STORAGES
     private val sc = SOCIAL_CLASSES
+    private val sch = SCHOLARS
 
     fun saveGeneratedBodyPart(dto: NewBodyPartDto): Long {
         val r = dslContext.newRecord(bp)
@@ -32,10 +34,10 @@ class BodyPartRepository(private val dslContext: DSLContext, private val bodyPar
     fun findBodyPartsByStorageId(storageId: Long): List<BodyPart> {
         return dslContext.select(bp, bpt, sc)
             .from(bp)
-            .join(s).on(s.STORAGE_ID.eq(bp.STORAGE_ID))
+            .join(st).on(st.STORAGE_ID.eq(bp.STORAGE_ID))
             .join(bpt).on(bpt.BODY_PART_TEMPLATE_ID.eq(bp.BODY_PART_TEMPLATE_ID))
             .join(sc).on(sc.SOCIAL_CLASS_ID.eq(bpt.SOCIAL_CLASS_ID))
-            .where(s.STORAGE_ID.eq(storageId))
+            .where(st.STORAGE_ID.eq(storageId))
             .fetch()
             .map { bodyPartMapper.fromBodyPartRecordToModel(it.value1(), it.value2(), it.value3()) }
     }
@@ -49,4 +51,40 @@ class BodyPartRepository(private val dslContext: DSLContext, private val bodyPar
             .fetch()
             .map { bodyPartMapper.fromBodyPartRecordToModel(it.value1(), it.value2(), it.value3()) }
     }
+
+    fun findBodyPartsByScholarId(scholarId: Long): List<BodyPart> {
+        return dslContext.select(bp, bpt, sc)
+            .from(bp)
+            .join(sch).on(sch.SCHOLAR_ID.eq(bp.SCHOLAR_ID))
+            .join(bpt).on(bpt.BODY_PART_TEMPLATE_ID.eq(bp.BODY_PART_TEMPLATE_ID))
+            .join(sc).on(sc.SOCIAL_CLASS_ID.eq(bpt.SOCIAL_CLASS_ID))
+            .where(sch.SCHOLAR_ID.eq(scholarId)).and(bp.STORAGE_ID.isNull).and(bp.SET_BODY_PARTS_ID.isNull)
+            .fetch()
+            .map { bodyPartMapper.fromBodyPartRecordToModel(it.value1(), it.value2(), it.value3()) }
+    }
+
+    fun existsBodyPartIdsForScholarId(bodyPartIds: Set<Long>, scholarId: Long): Boolean {
+        val count = dslContext.selectCount()
+            .from(bp)
+            .where(bp.BODY_PART_ID.`in`(bodyPartIds).and(bp.SCHOLAR_ID.eq(scholarId)))
+            .fetchOneInto(Int::class.java) ?: 0
+
+        return count == bodyPartIds.size
+    }
+
+    fun updateBodyPartLocationToStorage(bodyPartIds: Set<Long>, storageId: Long) {
+        val records = dslContext.selectFrom(bp)
+            .where(bp.BODY_PART_ID.`in`(bodyPartIds))
+            .fetch()
+
+        records.map { r ->
+            r.storageId = storageId
+            r.scholarId = null
+            r.setBodyPartsId = null
+        }
+
+        dslContext.batchStore(records).execute()
+    }
+
+
 }
