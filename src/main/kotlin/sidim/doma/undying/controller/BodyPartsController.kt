8543a9b933f4eb5e33d102e509f78e0758bb2f1f
ath31st <ath31st@yandex.ru>
2024-02-20 17:13:59 +0300

    saving and transfer body parts after finding v3 add BodyPartsController

---
+++ src/main/kotlin/sidim/doma/undying/controller/BodyPartsController.kt
@@ -0,0 +1,30 @@
+package sidim.doma.undying.controller
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import sidim.doma.undying.dto.storage.TransferBodyPartsDto
+import sidim.doma.undying.service.ActionService
+import sidim.doma.undying.service.monster.BodyPartService
+
+@RestController
+@Validated
+@RequestMapping("/api/v1/body_parts")
+class BodyPartsController(
+    private val bodyPartService: BodyPartService,
+    private val actionService: ActionService,
+) {
+    @PostMapping("/transfer_body_parts_to_storage")
+    @Transactional
+    fun transferBodyPartsToStorage(@RequestBody dto: TransferBodyPartsDto): ResponseEntity<HttpStatus> {
+        actionService.checkExistsPlayerAction(dto.scholarId, dto.actionUuid)
+        bodyPartService.transferBodyPartsFromScholarToStorage(dto)
+        //actionService.deleteActionUuidByScholarId(dto.scholarId, dto.actionUuid)
+        return ResponseEntity.ok(HttpStatus.OK)
+    }
+}
