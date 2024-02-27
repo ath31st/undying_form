48e8b36ec6a32011853c5d45c8312924ac1530db
ath31st <ath31st@yandex.ru>
2024-02-27 23:54:34 +0300

    add BodyPartShort

---
+++ src/main/kotlin/sidim/doma/undying/model/BodyPartShort.kt
@@ -0,0 +1,11 @@
+package sidim.doma.undying.model
+
+data class BodyPartShort(
+    val id: Long,
+    val quality: Int,
+    val integrity: Int,
+    val side: String?,
+    val nameBodyPartTemplate: String,
+)
+
+
