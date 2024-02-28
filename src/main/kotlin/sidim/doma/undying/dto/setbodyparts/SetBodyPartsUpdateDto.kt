fae060c6204816e836678bc6101d483cbe904d84
ath31st <ath31st@yandex.ru>
2024-02-28 22:48:36 +0300

    add SetBodyPartsUpdateDto

---
+++ src/main/kotlin/sidim/doma/undying/dto/setbodyparts/SetBodyPartsUpdateDto.kt
@@ -0,0 +1,11 @@
+package sidim.doma.undying.dto.setbodyparts
+
+class SetBodyPartsUpdateDto(
+    var setBodyPartsId: Long,
+    var leftHandIdForSlot: Long?,
+    var rightHandIdForSlot: Long?,
+    var leftLegIdForSlot: Long?,
+    var rightLegIdForSlot: Long?,
+    var upperBodyIdForSlot: Long?,
+    var headIdForSlot: Long?,
+)
