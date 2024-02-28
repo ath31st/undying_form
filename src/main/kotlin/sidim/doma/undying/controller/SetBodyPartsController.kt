0d5929be299ece78aede5135158e48d217e4b521
ath31st <ath31st@yandex.ru>
2024-02-28 22:49:03 +0300

    add SetBodyPartsController and endpoint for updating slots in set

---
+++ src/main/kotlin/sidim/doma/undying/controller/SetBodyPartsController.kt
@@ -0,0 +1,21 @@
+package sidim.doma.undying.controller
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import sidim.doma.undying.dto.setbodyparts.SetBodyPartsUpdateDto
+import sidim.doma.undying.service.monster.SetBodyPartsService
+
+@RestController
+@Validated
+@RequestMapping("/api/v1/set_body_parts")
+class SetBodyPartsController(private val setBodyPartsService: SetBodyPartsService) {
+    @PostMapping("/update_slots")
+    fun updateSetBodyParts(@RequestBody setBodyPartsUpdateDto: SetBodyPartsUpdateDto): ResponseEntity<HttpStatus> {
+        setBodyPartsService.
+    }
+}
