558188e976ea95683808cea0f20f8dd131aea7a5
ath31st <ath31st@yandex.ru>
2024-02-28 11:41:45 +0300

    add mapping in SetBodyPartsMapper

--- src/main/kotlin/sidim/doma/undying/mapper/SetBodyPartsMapper.kt
+++ src/main/kotlin/sidim/doma/undying/mapper/SetBodyPartsMapper.kt
@@ -1,24 +1,36 @@
 package sidim.doma.undying.mapper
 
+import org.jooq.Record7
 import org.springframework.stereotype.Component
+import sidim.doma.undying.generated.tables.records.BodyPartsRecord
 import sidim.doma.undying.generated.tables.records.SetsBodyPartsRecord
 import sidim.doma.undying.generated.tables.references.SETS_BODY_PARTS
 import sidim.doma.undying.model.SetBodyParts
 
 @Component
-class SetBodyPartsMapper {
+class SetBodyPartsMapper(private val bodyPartMapper: BodyPartMapper) {
     private val sbp = SETS_BODY_PARTS
-    fun fromRecordToModel(r1: SetsBodyPartsRecord): SetBodyParts {
+
+    fun fromRecordToModel(
+        rec: Record7<SetsBodyPartsRecord,
+                BodyPartsRecord,
+                BodyPartsRecord,
+                BodyPartsRecord,
+                BodyPartsRecord,
+                BodyPartsRecord,
+                BodyPartsRecord>
+    ): SetBodyParts {
         return SetBodyParts(
-            setBodyPartsId = r1[sbp.SET_BODY_PARTS_ID] ?: 0,
-            bodyPartsCount = r1[sbp.BODY_PARTS_COUNT] ?: 0,
-            setIsCompleted = r1[sbp.SET_IS_COMPLETED] ?: false,
-            bonusSet = r1[sbp.BONUS_SET] ?: 0,
-            leftHandSlot = null,
-            rightLegSlot = null,
-            upperBodySlot = null,
-            headSlot = null,
-            //todo add here body parts mapper
+            setBodyPartsId = rec.value1()[sbp.SET_BODY_PARTS_ID] ?: 0,
+            bodyPartsCount = rec.value1()[sbp.BODY_PARTS_COUNT] ?: 0,
+            setIsCompleted = rec.value1()[sbp.SET_IS_COMPLETED] ?: false,
+            bonusSet = rec.value1()[sbp.BONUS_SET] ?: 0,
+            leftHandSlot = bodyPartMapper.fromBodyPartRecordToShortModel(rec.value2()),
+            rightHandSlot = bodyPartMapper.fromBodyPartRecordToShortModel(rec.value3()),
+            leftLegSlot = bodyPartMapper.fromBodyPartRecordToShortModel(rec.value4()),
+            rightLegSlot = bodyPartMapper.fromBodyPartRecordToShortModel(rec.value5()),
+            upperBodySlot = bodyPartMapper.fromBodyPartRecordToShortModel(rec.value6()),
+            headSlot = bodyPartMapper.fromBodyPartRecordToShortModel(rec.value7()),
         )
     }
 }
