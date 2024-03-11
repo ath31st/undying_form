5791991c0300a0e820136e90932effd6ecf017ed
ath31st <ath31st@yandex.ru>
2024-03-11 19:42:29 +0300

    update endpoint in controller with new methods

--- src/main/kotlin/sidim/doma/undying/controller/SetBodyPartsController.kt
+++ src/main/kotlin/sidim/doma/undying/controller/SetBodyPartsController.kt
@@ -2,6 +2,7 @@ package sidim.doma.undying.controller
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import sidim.doma.undying.dto.setbodyparts.SetBodyPartsUpdateDto
 import sidim.doma.undying.service.StorageService
+import sidim.doma.undying.service.monster.BodyPartService
 import sidim.doma.undying.service.monster.SetBodyPartsService
 
 @RestController
@@ -17,16 +19,18 @@ import sidim.doma.undying.service.monster.SetBodyPartsService
 class SetBodyPartsController(
     private val setBodyPartsService: SetBodyPartsService,
     private val storageService: StorageService,
-    private val bodyPartsService: SetBodyPartsService
+    private val bodyPartService: BodyPartService
 ) {
+    @Transactional
     @PostMapping("/update_slots")
     fun updateSetBodyParts(@RequestBody dto: SetBodyPartsUpdateDto): ResponseEntity<HttpStatus> {
         setBodyPartsService.checkIsSetBodyPartsExistById(dto.setBodyPartsId)
         setBodyPartsService.checkUniqueBodyPartsInUpdateDto(dto)
         val scholarId = setBodyPartsService.getScholarIdBySetBodyParts(dto.setBodyPartsId)
         storageService.checkExistsBodyPartIdsInStorageByScholarId(dto, scholarId)
-        val bpIdsForDeletingAndUpdateing = setBodyPartsService.updateSlotsSetBodyParts(dto)
-
+        val bpIdsForDeletingAndUpdating = setBodyPartsService.updateSlotsSetBodyParts(dto)
+        bodyPartService.deleteExtraBodyPartsAfterUpdateSet(bpIdsForDeletingAndUpdating.first)
+        bodyPartService.transferBodyPartsFromStorageToSet(bpIdsForDeletingAndUpdating.second, dto.setBodyPartsId)
 
         return ResponseEntity.ok(HttpStatus.INTERNAL_SERVER_ERROR)
     }
