d43a6112632fdbf045ba08eb608088bf5eb8c247
ath31st <ath31st@yandex.ru>
2024-03-18 17:38:17 +0300

    add endpoint for deleting body parts from storage

--- src/main/kotlin/sidim/doma/undying/controller/BodyPartController.kt
+++ src/main/kotlin/sidim/doma/undying/controller/BodyPartController.kt
@@ -4,12 +4,14 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import sidim.doma.undying.dto.storage.TransferBodyPartsDto
+import sidim.doma.undying.dto.bodyparts.TransferBodyPartsDto
 import sidim.doma.undying.service.ActionService
+import sidim.doma.undying.service.StorageService
 import sidim.doma.undying.service.monster.BodyPartService
 
 @RestController
@@ -18,6 +20,7 @@ import sidim.doma.undying.service.monster.BodyPartService
 class BodyPartController(
     private val bodyPartService: BodyPartService,
     private val actionService: ActionService,
+    private val storageService: StorageService,
 ) {
     @PostMapping("/transfer_body_parts_to_storage")
     @Transactional
@@ -27,5 +30,13 @@ class BodyPartController(
         bodyPartService.deleteExtraBodyPartsAfterTransfer(dto.scholarId)
         actionService.deleteActionUuidByScholarId(dto.scholarId, dto.actionUuid)
         return ResponseEntity.ok(HttpStatus.OK)
+    }
+
+    @DeleteMapping("/delete_body_parts_from_storage")
+    fun deleteBodyPartsFromStorage(@RequestBody dto: TransferBodyPartsDto): ResponseEntity<HttpStatus> {
+        actionService.checkIfNoExistsPlayerAction(dto.scholarId, dto.actionUuid)
+        val storageId = storageService.getStorageIdByScholarId(dto.scholarId)
+        bodyPartService.deleteBodyPartsFromStorage(storageId, dto.bodyPartIds)
+        return ResponseEntity.ok(HttpStatus.NO_CONTENT)
     }
 }
