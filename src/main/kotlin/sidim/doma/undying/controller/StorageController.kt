f4a545bf6d5e44c523e5e3a054adca3804a7035f
ath31st <ath31st@yandex.ru>
2024-03-23 10:57:19 +0300

    add endpoint for deleting bp from storage

--- src/main/kotlin/sidim/doma/undying/controller/StorageController.kt
+++ src/main/kotlin/sidim/doma/undying/controller/StorageController.kt
@@ -1,22 +1,36 @@
 package sidim.doma.undying.controller
 
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import sidim.doma.undying.dto.storage.DeleteBodyPartsDto
 import sidim.doma.undying.model.Storage
 import sidim.doma.undying.service.StorageService
+import sidim.doma.undying.service.monster.BodyPartService
 
 @RestController
 @Validated
 @RequestMapping("/api/v1/storages")
 class StorageController(
     private val storageService: StorageService,
+    private val bodyPartService: BodyPartService,
 ) {
     @GetMapping("/storage_contents_by_scholar_id/{id}")
     fun getStorageContentsByScholarId(@PathVariable id: Long): ResponseEntity<Storage> {
         return ResponseEntity.ok(storageService.getStorageByScholarId(id))
+    }
+
+    @DeleteMapping("/delete_body_parts_from_storage")
+    fun deleteBodyPartsFromStorage(@RequestBody dto: DeleteBodyPartsDto): ResponseEntity<HttpStatus> {
+        val storageId = storageService.getStorageIdByScholarId(dto.scholarId)
+        val countDeletedBodyParts = bodyPartService.deleteBodyPartsFromStorage(storageId, dto.bodyPartIds)
+        storageService.increaseCountEmptySlotsByStorageId(storageId, countDeletedBodyParts)
+        return ResponseEntity.ok(HttpStatus.NO_CONTENT)
     }
 }
