0ae7d031994da752d96965ad311dda944086d330
ath31st <ath31st@yandex.ru>
2024-04-01 10:04:17 +0300

    integrate NamingService into RecipeBookService

--- src/main/kotlin/sidim/doma/undying/service/RecipeBookService.kt
+++ src/main/kotlin/sidim/doma/undying/service/RecipeBookService.kt
@@ -3,13 +3,22 @@ package sidim.doma.undying.service
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import sidim.doma.undying.exceptionhandler.exception.RecipeBookException
+import sidim.doma.undying.generated.tables.pojos.RecipeBooks
 import sidim.doma.undying.repository.RecipeBooksRepository
 
 @Service
-class RecipeBookService(private val recipeBooksRepository: RecipeBooksRepository) {
+class RecipeBookService(
+    private val recipeBooksRepository: RecipeBooksRepository,
+    private val namingService: NamingService
+) {
     fun checkExistsRecipeBookByUserId(userId: Long) {
         if (recipeBooksRepository.isRecipeBookExistByUserId(userId)) {
             throw RecipeBookException("Recipe book for user with id: $userId already exists", HttpStatus.CONFLICT)
         }
+    }
+
+    fun createEmptyRecipeBook(): RecipeBooks {
+        val title = namingService.generateRecipeBookTitle()
+        return recipeBooksRepository.saveEmptyRecipeBook(title)
     }
 }
