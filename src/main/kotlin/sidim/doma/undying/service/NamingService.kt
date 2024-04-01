acdbaad1ef3a1286261c32d716c8fc30b367d995
ath31st <ath31st@yandex.ru>
2024-04-01 10:02:53 +0300

    update NamingService with recipe book adjectives and nouns

--- src/main/kotlin/sidim/doma/undying/service/NamingService.kt
+++ src/main/kotlin/sidim/doma/undying/service/NamingService.kt
@@ -14,12 +14,14 @@ import sidim.doma.undying.exceptionhandler.exception.NamingException
 @Service
 class NamingService {
     private val csvPath = "classpath:csv_data/"
-    private val adjectiveFile = csvPath + "hideout_adjectives.csv"
-    private val nounFile = csvPath + "hideout_nouns.csv"
+    private val hideoutAdjectiveFile = csvPath + "hideout_adjectives.csv"
+    private val hideoutNounFile = csvPath + "hideout_nouns.csv"
     private val caretakerFirstNameFile = csvPath + "caretaker_firstname.csv"
     private val caretakerLastNameFile = csvPath + "caretaker_lastname.csv"
     private val scholarFirstNameFile = csvPath + "scholar_firstname.csv"
     private val scholarLastNameFile = csvPath + "scholar_lastname.csv"
+    private val recipeBookAdjectiveFile = csvPath + "recipe_book_adjectives.csv"
+    private val recipeBookNounFile = csvPath + "recipe_book_nouns.csv"
 
     fun generateCaretakerFirstLastName(): String {
         val firstName = getRandomLine(caretakerFirstNameFile)
@@ -35,13 +37,20 @@ class NamingService {
         return "$firstName $lastName"
     }
 
+    fun generateRecipeBookTitle(): String {
+        val adjective = getRandomLine(recipeBookAdjectiveFile)
+        val noun = getRandomLine(recipeBookNounFile)
+
+        return "$adjective $noun"
+    }
+
     fun generateHideoutName(): String {
         var adjective: String
         var noun: String
 
         do {
-            adjective = getRandomLine(adjectiveFile)
-            noun = getRandomLine(nounFile)
+            adjective = getRandomLine(hideoutAdjectiveFile)
+            noun = getRandomLine(hideoutNounFile)
         } while (checkGender(adjective) != checkGender(noun))
 
         return "${adjective.replaceFirstChar { it.titlecase(Locale.getDefault()) }} $noun"
