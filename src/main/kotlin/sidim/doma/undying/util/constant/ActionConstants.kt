0af74c35949bc3066fd97a36f2621d9426baefab
ath31st <ath31st@yandex.ru>
2024-04-04 09:49:47 +0300

    change constraint for user table and temporary change searching duration from 420 minutes to 1

--- src/main/kotlin/sidim/doma/undying/util/constant/ActionConstants.kt
+++ src/main/kotlin/sidim/doma/undying/util/constant/ActionConstants.kt
@@ -7,5 +7,5 @@ object ActionConstants {
     const val CHANCE_LOW_VALUE_CAUNT = 50
     const val CHANCE_MID_VALUE_CAUNT = 35
     const val CHANCE_HIGH_VALUE_CAUNT = 15
-    const val DURATION_FINDING_BODY_PARTS = 420L
+    const val DURATION_FINDING_BODY_PARTS = 1L // was 420L
 }
--- src/main/resources/db/migration/change_tables/V041__add_column_recipe_book_id_to_users.sql
+++ src/main/resources/db/migration/change_tables/V041__add_column_recipe_book_id_to_users.sql
@@ -1,2 +1,2 @@
 ALTER TABLE users
-    ADD COLUMN recipe_book_id BIGINT UNIQUE REFERENCES recipe_books (recipe_book_id);
+    ADD COLUMN recipe_book_id BIGINT UNIQUE NOT NULL REFERENCES recipe_books (recipe_book_id) default -1;
