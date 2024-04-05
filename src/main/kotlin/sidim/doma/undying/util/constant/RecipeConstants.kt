771371279c40773f6a63b78a14f5511f4f947619
ath31st <ath31st@yandex.ru>
2024-04-05 21:32:12 +0300

    add RecipeConstants for new entity

---
+++ src/main/kotlin/sidim/doma/undying/util/constant/RecipeConstants.kt
@@ -0,0 +1,11 @@
+package sidim.doma.undying.util.constant
+
+object RecipeConstants {
+    const val LOW_RARITY = 1
+    const val MID_RARITY = 2
+    const val HIGH_RARITY = 3
+    const val CHANCE_LOW_RARITY = 75
+    const val CHANCE_MID_RARITY = 20
+    const val CHANCE_HIGH_RARITY = 5
+    const val MAX_QUANTITY = 100
+}
