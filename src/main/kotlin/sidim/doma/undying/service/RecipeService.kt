2a50df29da77c97ba2826fcb38da852074c0467d
ath31st <ath31st@yandex.ru>
2024-04-12 22:20:28 +0300

    add initial RecipeService

---
+++ src/main/kotlin/sidim/doma/undying/service/RecipeService.kt
@@ -0,0 +1,15 @@
+package sidim.doma.undying.service
+
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Service
+import sidim.doma.undying.mapper.RecipeMapper
+import sidim.doma.undying.repository.RecipeRepository
+
+@Service
+class RecipeService(private val recipeRepository: RecipeRepository, private val recipeMapper: RecipeMapper) {
+    fun checkExistsRecipeById(recipeId: Int) {
+        if (recipeRepository.isRecipeExistById(recipeId)) {
+            throw RecipeException("Recipe with id: $recipeId already exists", HttpStatus.CONFLICT)
+        }
+    }
+}
