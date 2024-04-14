52edad64b5815848112a36b6a563fec58aa89c43
ath31st <ath31st@yandex.ru>
2024-04-14 09:38:21 +0300

    fix recipe mapper

--- src/main/kotlin/sidim/doma/undying/mapper/RecipeMapper.kt
+++ src/main/kotlin/sidim/doma/undying/mapper/RecipeMapper.kt
@@ -9,13 +9,13 @@ import sidim.doma.undying.generated.tables.records.RecipesRecord
 import sidim.doma.undying.model.Recipe
 
 @Component
-class RecipeMapper() {
-    fun fromRecipePojoToModel(pair: Pair<RecipesRecord, Result<Record1<ItemsRecord>>>): Recipe {
+class RecipeMapper {
+    fun fromRecipePojoToModel(pair: Pair<RecipesRecord?, Result<Record1<ItemsRecord>>>): Recipe {
         val r = pair.first
         val items = pair.second
 
         return Recipe(
-            recipeId = r.recipeId ?: 0,
+            recipeId = r!!.recipeId ?: 0,
             name = r.name ?: "",
             description = r.description ?: "",
             rarity = r.rarity ?: 0,
