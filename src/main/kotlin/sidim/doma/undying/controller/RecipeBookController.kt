35242344f51e61a2ea30625f7121e56ced016f82
ath31st <ath31st@yandex.ru>
2024-04-16 09:38:22 +0300

    add getting a recipe book from controller

--- src/main/kotlin/sidim/doma/undying/controller/RecipeBookController.kt
+++ src/main/kotlin/sidim/doma/undying/controller/RecipeBookController.kt
@@ -1,9 +1,20 @@
 package sidim.doma.undying.controller
 
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import sidim.doma.undying.service.RecipeBookService
 
 @RestController
+@Validated
+@RequestMapping("/api/v1/recipe_books")
 class RecipeBookController(val recipeBookService: RecipeBookService) {
 
+    @GetMapping("/recipe_book/{userId}")
+    fun getRecipeBookByUserId(@PathVariable userId: Long): ResponseEntity<Any> {
+        return ResponseEntity.ok(recipeBookService.getRecipeBooksByUserId(userId))
+    }
 }
