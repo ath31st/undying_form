1dfa0a0c5b0438bae9a03936721ae4e997d15cd5
ath31st <ath31st@yandex.ru>
2024-04-18 10:29:32 +0300

    add CityService and methods from repository

---
+++ src/main/kotlin/sidim/doma/undying/service/location/CityService.kt
@@ -0,0 +1,26 @@
+package sidim.doma.undying.service.location
+
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Service
+import sidim.doma.undying.dto.citiy.NewCityDto
+import sidim.doma.undying.exceptionhandler.exception.CityException
+import sidim.doma.undying.generated.tables.pojos.Cities
+import sidim.doma.undying.repository.location.CityRepository
+
+@Service
+class CityService(private val cityRepository: CityRepository) {
+    fun getCityById(cityId: Int): Cities {
+        return cityRepository.findCityById(cityId)
+            ?: throw CityException("City with id: $cityId not found", HttpStatus.NOT_FOUND)
+    }
+
+    fun createCity(dto: NewCityDto): Cities {
+        return cityRepository.saveNewCity(dto)
+    }
+
+    fun checkCityExistByName(cityName: String) {
+        if (cityRepository.isCityExistByName(cityName)) {
+            throw CityException("City with name: $cityName already exists", HttpStatus.CONFLICT)
+        }
+    }
+}
