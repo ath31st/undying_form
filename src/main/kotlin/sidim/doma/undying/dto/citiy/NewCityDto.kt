79febf8e7827c44d7b93f451e4e5179525ab8586
ath31st <ath31st@yandex.ru>
2024-04-18 09:54:51 +0300

    add dto for creation new city

---
+++ src/main/kotlin/sidim/doma/undying/dto/citiy/NewCityDto.kt
@@ -0,0 +1,9 @@
+package sidim.doma.undying.dto.citiy
+
+class NewCityDto(
+    val name: String,
+    val population: Int,
+    val description: String,
+    val foundationYear: Int,
+    val flagUrl: String,
+)
