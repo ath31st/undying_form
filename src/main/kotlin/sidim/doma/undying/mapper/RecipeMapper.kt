2bd3e8da1ce9e63c6cbfeec0b805d30b4045354a
ath31st <ath31st@yandex.ru>
2024-05-03 20:53:57 +0300

    upgrade recipe mapper with third record from repository

--- src/main/kotlin/sidim/doma/undying/mapper/RecipeMapper.kt
+++ src/main/kotlin/sidim/doma/undying/mapper/RecipeMapper.kt
@@ -3,23 +3,44 @@ package sidim.doma.undying.mapper
 import org.jooq.Record1
 import org.jooq.Result
 import org.springframework.stereotype.Component
+import sidim.doma.undying.dto.item.ItemForRecipeDto
 import sidim.doma.undying.generated.tables.pojos.Items
 import sidim.doma.undying.generated.tables.records.ItemsRecord
+import sidim.doma.undying.generated.tables.records.RecipeItemsRecord
 import sidim.doma.undying.generated.tables.records.RecipesRecord
 import sidim.doma.undying.model.Recipe
 
 @Component
 class RecipeMapper {
-    fun fromRecipePojoToModel(pair: Pair<RecipesRecord?, Result<Record1<ItemsRecord>>>): Recipe {
-        val r = pair.first
-        val items = pair.second
+    fun fromRecipePojoToModel(
+        triple: Triple<RecipesRecord?,
+                Result<Record1<ItemsRecord>>,
+                Result<Record1<RecipeItemsRecord>>>
+    ): Recipe {
+        val r = triple.first
+        val items = triple.second
+        val quantityItems = triple.third
+
+        val ingredients = items.map { itemRecord ->
+            val itemId = itemRecord.into(Items::class.java).itemId ?: 0
+            val recipeItemRecord = quantityItems.find { it.value1().itemId == itemId }
+            val quantity = recipeItemRecord?.value1()?.quantity ?: 0
+
+            ItemForRecipeDto(
+                itemId = itemId,
+                name = itemRecord.into(Items::class.java).name ?: "",
+                description = itemRecord.into(Items::class.java).description ?: "",
+                rarity = itemRecord.into(Items::class.java).rarity ?: 0,
+                quantity = quantity
+            )
+        }.toList()
 
         return Recipe(
             recipeId = r!!.recipeId ?: 0,
             name = r.name ?: "",
             description = r.description ?: "",
             rarity = r.rarity ?: 0,
-            ingredients = items.map { it.into(Items::class.java) }
+            ingredients = ingredients
         )
     }
 }
