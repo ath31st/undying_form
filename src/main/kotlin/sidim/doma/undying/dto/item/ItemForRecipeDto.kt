3c34db4daa3c80b11466e5a228062d2979048d97
ath31st <ath31st@yandex.ru>
2024-05-03 20:51:45 +0300

    refactoring ItemForRecipeDto for a full recipe model

--- src/main/kotlin/sidim/doma/undying/dto/item/ItemForRecipeDto.kt
+++ src/main/kotlin/sidim/doma/undying/dto/item/ItemForRecipeDto.kt
@@ -2,6 +2,9 @@ package sidim.doma.undying.dto.item
 
 class ItemForRecipeDto(
     val itemId: Int,
+    val name: String,
+    val description: String,
+    val rarity: Int,
     val quantity: Int,
 )
 
